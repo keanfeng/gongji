bb665f75e986bdabc81843082907f522c01daadf
keanfeng <keanfeng@sina.com>
2017-11-09 17:00:11 +0800

    commit

--- netty/DataPackage.java
+++ netty/DataPackage.java
@@ -2,12 +2,35 @@ package com.my.test.bean;
 
 public class DataPackage {
 
-	
+	//包头标记
 	private static String PACKAGE_HEAD_MARK = "ffffffff";
+	private static int PACKAGE_HEAD_MARK_NUM = 4; 
+	
+	//协议版本
 	private static String PROTOCOL_VERSION = "01000001";
+	private static int PROTOCOL_VERSION_NUM = 4;
+	
+	//命令类型
 	private static String COMMAND_TYPE = "ffffffff";
+	private static int COMMAND_TYPE_NUM = 4;
 	
+	//包体长度
+	private static int PACKAGE_LENGTH_NUM = 4;
 	
+	//保留字段
+	private static String RESERVE = "ffffffffffffffffffffffffffffffff";
+	private static int RESERVE_NUM = 16;
 	
+	/**
+	 * 包解析入口方法
+	 * @param data
+	 * @return
+	 */
+	public static byte[] analysis(byte[] data){
+		
+	}
 	
+	public static boolean checkPackageHead(){
+		
+	}
 }
